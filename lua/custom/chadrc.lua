f3f07da435dd1d6b4fa463bd83b7bc269d5478a3
suryansh-dey <deysuryansh@gmail.com>
2024-11-16 16:41:47 +0530

    removed seperator from context

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,5 +1,5 @@
 local M = {}
-M.ui = { theme = 'dark_horizon' }
+M.ui = { theme = 'onedark' }
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
 vim.opt.relativenumber = true
--- lua/custom/configs/treesitter_context.lua
+++ lua/custom/configs/treesitter_context.lua
@@ -5,5 +5,5 @@ TSContext.setup({
   trim_scope = "outer",        -- Which context lines to discard
   min_window_height = 0,       -- Minimum editor window height to enable context
   mode = "topline",            -- Line used to calculate context; options: 'cursor', 'topline'
-  separator = '_',             -- Separator between context and content; pass a string for a custom separator
+  separator = nil,             -- Separator between context and content; pass a string for a custom separator
 })
