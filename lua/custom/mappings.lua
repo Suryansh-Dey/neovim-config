47cd8abd4f9fd500fc7f5a776cca7ca697985d55
suryansh-dey <deysuryansh@gmail.com>
2024-11-21 07:30:13 +0530

    changed a keybinding

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,7 +1,8 @@
+-- ThePrimeagen's remap
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -58,6 +59,6 @@ end, { desc = "Debugger step_into" })
 vim.keymap.set("n", "<leader>do", function()
   require('dap').step_out()
 end, { desc = "Debugger step_out" })
-vim.keymap.set("n", "<leader>de", function()
+vim.keymap.set("n", "<leader>dx", function()
   require('dap').terminate()
 end, { desc = "Debugger terminate" })
