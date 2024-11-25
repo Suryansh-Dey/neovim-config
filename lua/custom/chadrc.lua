788d91d9d7da7bc8112afaf0a8914dd24e529d1e
suryansh-dey <deysuryansh@gmail.com>
2024-11-25 22:57:21 +0530

    readded M.maaping.crates

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,4 +1,17 @@
 local M = {}
 M.ui = { theme = 'catppuccin' }
 M.plugins = "custom.plugins"
+M.mappings = {
+  crates = {
+    plugin = true,
+    n = {
+      ["<leader>rcu"] = {
+        function()
+          require('crates').upgrade_all_crates()
+        end,
+        "update crates"
+      }
+    }
+  }
+}
 return M
