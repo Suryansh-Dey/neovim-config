d12dffee8bfec77a27d9e665fa78750347b4f083
suryansh-dey <deysuryansh@gmail.com>
2024-12-23 18:20:24 +0530

    Fixing last commit

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -5,6 +5,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local lspconfig = require("lspconfig")
 
 local setup_arg = {
+  on_attach = on_attach,
   capabilities = capabilities
 }
 
