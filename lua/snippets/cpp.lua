8f98e931c45d4978c984c2076cc257b3cd8afa69
Suryansh-Dey <deysuryansh@gmail.com>
2025-08-10 12:30:57 +0530

    C-b to go back by word

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -2,25 +2,29 @@ local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
 
-local snippet = { s("cp", {
-    t({ "#include <bits/stdc++.h>",
-        "using namespace std;",
-        "#define int long long int",
-        "",
-        "signed main() {",
-        "\tstd::ios::sync_with_stdio(false);",
-        "\tstd::cin.tie(nullptr);",
-        "\tstd::cout.tie(nullptr);",
-        "\tint tc;",
-        "\tcin>>tc;",
-        "\twhile(tc--) {",
-        "\t\t" }),
-    i(0),
-    t({ "",
-        "\t}",
-        "}" })
-}
+local snippet = { s("cp", fmt([[
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void solve() {{
+    {}
+}}
+
+signed main() {{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while (tc--)
+        solve();
+}}
+]], {
+    i(0)
+})
 ),
     s("fr", {
         t("for(int i=0;i<"), i(1), t(";i++) {"),
