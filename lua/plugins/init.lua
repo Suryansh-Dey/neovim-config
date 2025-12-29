55fd9d343a1b23b27bd838280efd45c13233d828
Suryansh Dey <deysuryansh@gmail.com>
2025-12-29 10:17:34 +0530

    Fixing FlashLable highlight

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -243,7 +243,12 @@ return {
         "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        opts = {},
+        opts = {
+            on_highlights = function(hl, c)
+                hl.Substitute = { bg = c.cyan, fg = c.black }
+                hl.FlashLabel = { bg = c.cyan, fg = c.black }
+            end,
+        },
         config = function(_, opts)
             require("tokyonight").setup(opts)
             vim.cmd([[colorscheme tokyonight-night]])
