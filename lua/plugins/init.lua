6925f30046b4a2f8bcba25593b2a094c79905683
Suryansh Dey <deysuryansh@gmail.com>
2026-02-04 20:23:34 +0530

    Increase Telescope preview width

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,6 +21,18 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-treesitter/nvim-treesitter" },
         cmd = "Telescope",
+        opts = {
+            defaults = {
+                layout_strategy = 'horizontal',
+                layout_config = {
+                    horizontal = {
+                        preview_width = 0.7,
+                        results_width = 0.3,
+                    },
+                    width = 0.99
+                },
+            },
+        }
     },
     {
         "nvim-treesitter/nvim-treesitter",
